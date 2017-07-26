74a9be97c0d9675b6cc70905ef07cd29043000f5
StephenJWaite <swai013@aucklanduni.ac.nz>
2017-07-27 10:22:49 +1200

    Implemented controlPoint seeding and snap functions

--- ImageTools/createControlPoints.py
+++ ImageTools/createControlPoints.py
@@ -11,9 +11,9 @@ import imageTools as IT
 print 'Running createControlPoints'
 
 #Set up working directories
-workingDir = '/home/stephen/Documents/Python/imageToolsTestSpace/'
-imageDir = '/home/stephen/Documents/Python/imageToolsTestSpace/'
-contourDir = '/home/stephen/Documents/Python/imageToolsTestSpace/'
+workingDir = '/home/stephen/Documents/Python/myPythonTools/imageToolsTestSpace/'
+imageDir = '/home/stephen/Documents/Python/myPythonTools/imageToolsTestSpace/'
+contourDir = '/home/stephen/Documents/Python/myPythonTools/imageToolsTestSpace/'
 
 #Read in input files
 imageFile = mpimg.imread(imageDir + '6948_02.png')
@@ -22,21 +22,35 @@ contourData = np.loadtxt(contourDir + 'Contors.txt')
 print '    input contour size:', np.shape(contourData)
 
 basePlot = plt.figure()
-fuck = basePlot.add_subplot(111)
-fuck.imshow(imageFile)
+ax = basePlot.add_subplot(111)
+ax.imshow(imageFile)
 
 #rezero the slide numbers imageFile[:,2]
 startSlide = np.min(contourData[:,2])
 print 'Starting slide:',startSlide
 
 #loop through and plot the first contour
+firstContour=contourData[0,0:2].reshape(1,2)
 for i in range(len(contourData)):
 	if contourData[i,2] <= startSlide:
-		fuck.scatter(contourData[i,0]+5,contourData[i,1]+5)
+		firstContour=np.append(firstContour,contourData[i,0:2].reshape(1,2),axis=0)
 
-fuck.axis('equal')
+print 'firstContour.shape:', np.shape(firstContour)
+ax.plot(firstContour[:,0]+5,firstContour[:,1]+5)
 
-points = IT.pickPoints(3,basePlot)
+ax.axis('equal')
+
+#Set the number of master control Points
+MCP=4
+#Set the snap sphereRadius
+sR=20
+MPoints=np.zeros((MCP,2))
+for i in range(MCP):
+	cat=plt.ginput(1)
+	print 'Point group ',i ,':', cat[0]
+	MPoints[i,:]= np.asarray(cat)
+	ax.scatter(MPoints[i,0],MPoints[i,1], facecolors='none', edgecolors='g')
+	IT.snapSphere(firstContour,MPoints[i,:],sR)
 
 plt.show()
 
--- ImageTools/imageTools.py
+++ ImageTools/imageTools.py
@@ -2,6 +2,7 @@
 
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 def helloWorld():
 	print 'Hello Stephen, you nailed it'
@@ -230,30 +231,113 @@ def weightedGreyScale(imageData):
 
 	return grey
 
-def pickPoints(numPoints,fig):
-	if numPoints == 0:
-		freeSelect = True
-	else:
-		freeSelect = False
-		numPoints = numPoints
+def pickPoint(fig):
+
+	print 'running function pickPoint'
 
 	def onclick(event):
-		return event.x, event.y
+		print '+Running onclick event+'
+		print event.xdata, event.ydata
+		fig.canvas.mpl_disconnect(cid)
+		return event.xdata, event.ydata
+
 
 	cid = fig.canvas.mpl_connect('button_press_event', onclick)
+	return cid
 
-#	def __init__(self,numPoints,fig):
-#		if numPoints == 0:
-#			self.freeSelect = True
-#		else:
-#			self.freeSelect = False
-#			self.numPoints = numPoints
-#
-#		self.xs = list(fig.get_xdata())
-#
-#		if not self.freeSelect:
-#			for i in range(self.numPoints):
-#				self.cid = fig.figure.canvas.mpl_connect('button_press_event',self)
+#pickedPoints is a class for selecting and storing x,y coordinates from user selected locations on a figure.
+#the class takes the number of points that the user wants to select and the figure handle for the figure 
+#they wish to select from. 
+#Still to implement, free form selection for unspecified number of points, breaks with a keystroke enter.
+class pickedPoints():
+	def __init__(self,numPoints,fig):
+		#initialise self varaibles
+		self.points=np.zeros((numPoints,2)) #array of x and y points
+		self.count=0 #counter used to determine what point number the user is at.
+		self.numPoints = numPoints# total number of points that the user will select
+		self.fig=fig #Figure handle for the figure the user will select from
+		self.freeSelect=False #no yet is use, true will be for free form selection
+		#create button event for function pickPoints
+		self.cid = self.fig.canvas.mpl_connect('button_press_event',self.pickPoints)
+
+	def pickPoints(self,event):
+		print 'Runing pickPoints from class pickedPoints'
+		#assigned mouse click x and y values to point array
+		self.points[self.count,:] = event.xdata,event.ydata
+		#increase point counter
+		self.count=self.count+1
+		print event.xdata, event.ydata
+		#excape condition, if point counter equals the number of points, disconnect button event
+		if self.count==self.numPoints:
+			self.fig.canvas.mpl_disconnect(self.cid)
+
+class classTest():
+	def __init__(self,test):
+		self.cat=10/test
+		print 'cat in class:',self.cat
+
+def snapSphere(contourData,point,sR):
+	#loop through the contour data, check if each point is inside the snap sphere
+	print 'Running snapSphere...'
+	sphereNodes=np.ones((1,2))*-1
+	for i in range(len(contourData)):
+		if (np.abs(contourData[i,0]-point[0])<=sR) and (np.abs(contourData[i,1]-point[1])<=sR):
+			print 'Point ', i, ': ', contourData[i,:]
+			dist=np.sqrt((contourData[i,0]-point[0])**2 + (contourData[i,1]-point[1])**2)
+			print 'Dist: ',dist
+			if sphereNodes[0,0]==-1:
+				sphereNodes[0,:]=[dist,i]
+			else:
+				sphereNodes=np.append(sphereNodes,[[dist,i]],axis=0)
+
+	#sort the sphereNodes
+	print 'Dist vector shape: ', np.shape(sphereNodes)
+	print 'Dist vector:', sphereNodes
+	sphereNodes=sphereNodes[np.argsort(sphereNodes[:,0]),:]
+	print 'Sorted Dist vector:', sphereNodes
+	#Calculate the distance between the two closets nodes
+	#nodeDist=(np.sqrt((contourData[sphereNodes[0,1],0]
+	#          -contourData[sphereNodes[1,1],0])**2
+	#          +(contourData[sphereNodes[0,1],1]
+	#          -contourData[sphereNodes[1,1],1])**2
+	#         ))
+	#Calculate the angle between contour nodes and the picked node
+	LinePoints=np.zeros((2,2))
+	LinePoints[0,:]=contourData[sphereNodes[0,1],:]
+	LinePoints[1,:]=contourData[sphereNodes[1,1],:]
+	xPt,yPt = projectPointToLine(LinePoints,point)
+	print 'xPt: ',xPt, ' yPt: ',yPt
+
+
+def projectPointToLine(Line,Point):
+	print 'Running projectPointToLine'
+	print 'Line shape: ', np.shape(Line)
+	print 'Line values: ', Line
+	print 'Point values: ', Point
+	#Calculate the gradient Mt (target) and constant Ct
+	Ct=(Line[0,1]*Line[1,0] - Line[1,1]*Line[0,0])/(Line[1,0]-Line[0,0])
+	Mt=(Line[0,1]-Ct)/Line[0,0]
+	#The gradient of the orthogonal line is the inverse of Mt
+	Ms=-1/Mt
+	#Given the point and the gradient, calculate the constant 
+	Cs=Point[1]-Point[0]*Ms
+	#Calculate the intercept between the two lines
+	xPt=(Cs-Ct)/(Mt-Ms)
+	yPt=Mt*xPt+Ct
+	print 'xPt: ',xPt, ' yPt: ',yPt
+
+	linePlot = plt.figure()
+	axLine = linePlot.add_subplot(111)
+	axLine.plot([Line[0,0],Line[1,0]],[Line[0,1],Line[1,1]])
+	axLine.scatter(Point[0],Point[1], facecolors='g', edgecolors='g')
+	axLine.scatter(xPt,yPt, facecolors='r', edgecolors='r')
+	axLine.plot([Point[0],xPt],[Point[1],yPt],color='r')
+	axLine.axis('equal')
+
+	return xPt,yPt
+
+
+
 
 
 
