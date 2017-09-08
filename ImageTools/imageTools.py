5405504c102bcf81717d6e31217febe75bef83f4
StephenJWaite <swai013@aucklanduni.ac.nz>
2017-09-08 17:32:35 +1200

    Added contour time loop to createControlPoints, fixed up a few pugs in Imagetools

--- ImageTools/imageTools.py
+++ ImageTools/imageTools.py
@@ -14,12 +14,12 @@ def readPLY(meshString):
 	#Step 2) Read until the end hear line, we are now down with text
 	f=open(meshString)
 
-	#Check that its ply 
+	#Check that its ply
 	line=f.readline().rstrip()
-	
+
 	if line!='ply':
 		print 'This is not a PLY file', line
-		
+
 		Nodes=0
 		Elems=0
 	else:
@@ -34,7 +34,7 @@ def readPLY(meshString):
 
 			if line.find('element face')!=-1:
 				elemNum=int(line[13:])
-				print 'Number of Elements:', elemNum	
+				print 'Number of Elements:', elemNum
 
 		print 'Reading Nodes...'
 		Nodes=np.zeros(shape=(nodeNum,3))
@@ -49,11 +49,11 @@ def readPLY(meshString):
 			line=f.readline().rstrip()
 			temp=[int(s) for s in line.split()]
 			Elems[x,:]=temp[1:]
-			
+
 		return Nodes, Elems
 
 def extractPointsFromYPlane(Nodes,Elems,yPos):
-	#This function is used to sort nodes on either side of 
+	#This function is used to sort nodes on either side of
 	#the yPos
 
 	numNodes=Nodes.shape[0]
@@ -67,14 +67,14 @@ def extractPointsFromYPlane(Nodes,Elems,yPos):
 		for y in range(0,3):
 			if Nodes[Elems[x,y],1] < yPos:
 				temp[y]= 1
-		#Check to see if that temp array is all 1s (<Pos) or 0s (>Pos)	
-		#print temp, sum(temp)	
+		#Check to see if that temp array is all 1s (<Pos) or 0s (>Pos)
+		#print temp, sum(temp)
 		if (sum(temp) > 0) and (sum(temp) < 3):
 			#print 'element spans the y plane'
 			if sum(temp)==1:
 				#we know that there is one point on the <yPos side
 				#Note, argsort defaul goes smallest->biggest, so our
-				#unique node is < Pos, ordered it will be the [2] 
+				#unique node is < Pos, ordered it will be the [2]
 				#position in the elemOrdered
 				elemOrdered=Elems[x,np.argsort(temp)]
 				p1x, p1z = calculateYintercept(Nodes[elemOrdered[2],:],Nodes[elemOrdered[0],:],yPos)
@@ -98,7 +98,7 @@ def extractPointsFromYPlane(Nodes,Elems,yPos):
 	return pointCloud
 
 def extractPointsFromZPlane(Nodes,Elems,zPos):
-	#This function is used to sort nodes on either side of 
+	#This function is used to sort nodes on either side of
 	#the yPos
 
 	numNodes=Nodes.shape[0]
@@ -112,14 +112,14 @@ def extractPointsFromZPlane(Nodes,Elems,zPos):
 		for y in range(0,3):
 			if Nodes[Elems[x,y],2] < zPos:
 				temp[y]= 1
-		#Check to see if that temp array is all 1s (<Pos) or 0s (>Pos)	
-		#print temp, sum(temp)	
+		#Check to see if that temp array is all 1s (<Pos) or 0s (>Pos)
+		#print temp, sum(temp)
 		if (sum(temp) > 0) and (sum(temp) < 3):
 			#print 'element spans the y plane'
 			if sum(temp)==1:
 				#we know that there is one point on the <yPos side
 				#Note, argsort defaul goes smallest->biggest, so our
-				#unique node is < Pos, ordered it will be the [2] 
+				#unique node is < Pos, ordered it will be the [2]
 				#position in the elemOrdered
 				elemOrdered=Elems[x,np.argsort(temp)]
 				#if isUnique(np.array([Nodes[elemOrdered[0],2],Nodes[elemOrdered[1],2],Nodes[elemOrdered[2],2]])):
@@ -179,7 +179,7 @@ def calculateYintercept(p1,p2,yPos):
 	zPos=((yPos-p2[1])*np.tan(zyTheta))+p2[2]
 
 	if np.isnan(xPos) or np.isnan(zPos):
-		print 'Fucking NaN cat' 
+		print 'Fucking NaN cat'
 		print p1
 		print p2
 		print 'x values:', xPos, xyTheta, 'angle crap ', (p1[0]-p2[0])/(p1[1]-p2[1])
@@ -190,7 +190,7 @@ def calculateYintercept(p1,p2,yPos):
 def calculateZintercept(p1,p2,zPos,elems):
 	#Function is used to calculate the x and z coordinates of a y
 	#intecept of two points
-    
+
 	#Start with xy plane
 	xzTheta = np.arctan((p1[0]-p2[0])/(p1[2]-p2[2]))
 	xPos=((zPos-p2[2])*np.tan(xzTheta))+p2[0]
@@ -200,14 +200,14 @@ def calculateZintercept(p1,p2,zPos,elems):
 	yPos=((zPos-p2[2])*np.tan(yzTheta))+p2[1]
 
 	if np.isnan(xPos) or np.isnan(yPos):
-		print 'Fucking NaN cat' 
+		print 'Fucking NaN cat'
 		print p1
 		print p2
 		print 'x values:', xPos, xzTheta, 'angle crap ', (p1[0]-p2[0])/(p1[1]-p2[1])
 		print 'y values:', yPos, yzTheta
 
 
-	if ((p1[2]-p2[2])==0.0): 
+	if ((p1[2]-p2[2])==0.0):
 	 	print 'DIV ZERO'
 	 	print p2
 		print 'x values:', xPos, xzTheta, 'angle crap ', (p1[0]-p2[0])/(p1[1]-p2[1])
@@ -246,8 +246,8 @@ def pickPoint(fig):
 	return cid
 
 #pickedPoints is a class for selecting and storing x,y coordinates from user selected locations on a figure.
-#the class takes the number of points that the user wants to select and the figure handle for the figure 
-#they wish to select from. 
+#the class takes the number of points that the user wants to select and the figure handle for the figure
+#they wish to select from.
 #Still to implement, free form selection for unspecified number of points, breaks with a keystroke enter.
 class pickedPoints():
 	def __init__(self,numPoints,fig):
@@ -298,8 +298,8 @@ def snapSphere(contourData,point,sR):
 		nodeGroup=np.asarray([sphereNodes[0,1],sphereNodes[0,1]+1,len(contourData)-1]).astype(int)
 	else:
 		nodeGroup=np.asarray([sphereNodes[0,1],sphereNodes[0,1]-1,sphereNodes[0,1]+1]).astype(int)
-	
-    #Create an empty array for the two end points spaning the user point  
+
+    #Create an empty array for the two end points spaning the user point
 	LinePoints=np.zeros((2,2))
 	pointPosition=1
     #Store the end points IS THE ORDER IMPORTANT?
@@ -314,7 +314,7 @@ def snapSphere(contourData,point,sR):
 		pointPosition=pointPosition+1
 		LinePoints[1,:]=contourData[nodeGroup[pointPosition],:]
 		xPt,yPt = projectPointToLine(LinePoints,point)
-		lineCheck=checkPointOnLineInterval(LinePoints,[xPt,yPt])	
+		lineCheck=checkPointOnLineInterval(LinePoints,[xPt,yPt])
 		#If the next segment also fales, our picked point is the nearest node
 		if not lineCheck:
 			xPt=contourData[nodeGroup[0],0]
@@ -325,7 +325,7 @@ def snapSphere(contourData,point,sR):
 		print 'Spanning end nodes'
 		return [xPt, yPt], len(contourData)-1
 	if nodeGroup[0]<nodeGroup[pointPosition]:
-		print 'left is smaller'
+		#print 'left is smaller'
 		return [xPt, yPt], nodeGroup[0]
 	else:
 		print 'right is smaller'
@@ -342,7 +342,7 @@ def sortContourData(contourData):
 
 	#set up a blank list
 	contours=[1]*count
-	print 'Number of contours:',count
+	print '\tNumber of contours:',count
 	#set a counter that will count the number of points in a contour
 	count=0
 	#set a boolean flag toggle to TRUE
@@ -354,13 +354,16 @@ def sortContourData(contourData):
 		#set the current working (temporal) contour
 		if toggle:
 			index=contourData[i,2].astype(int)
-			toggle=False 
+			toggle=False
 
+		#print 'i: ',i,'index',contourData[i,2]
 		if contourData[i,2]==index:
 			count=count+1
 		else:
+			#print 'Contour Number:',index,'Contour Size:',np.shape(contourData[(i-count):i,0:2]),'i:',i,'count',count,'i-count',(i-count)
 			contours[index]=contourData[(i-count):i,0:2]
 			toggle=True
+			count=1
 
 	return contours
 
@@ -368,7 +371,7 @@ def sortContourData(contourData):
 #equidistance intervals between Master points.
 #Dist: the current distance that the crawler is at for a slaveSegment. One it moves to a new slaveSegment, it resest its self to the remainder.
 #segDist:The distance of the current dataSegment (not this is a segment between raw data points, not the slave points we are seeding, thats slaveSegment.)
-#segSize: The distance of the current slaveSegment, this is some really shitty naming from me, my bad. 
+#segSize: The distance of the current slaveSegment, this is some really shitty naming from me, my bad.
 #MDist: the current distance for the whole master segment. resets itself when it moves to a new master point
 #CPpos: Current control point position (seed points, so could rename this aswell.)
 #CNP: Current Node Points, this stores the value of the current node ID from the data array, its used to tell when the crawler has made it to the next masterPoint.
@@ -377,39 +380,28 @@ def seedSlavePoints(distVector,contourData,MasterPoints,seedNumber,ax):
 	#pdb.set_trace()
 	print 'Running seedSlavePoints'
 	seedNumber=seedNumber+1
-	ControlPoints=np.zeros(((len(MasterPoints)+len(MasterPoints)*seedNumber),2))
+	ControlPoints=np.zeros(((len(MasterPoints)*seedNumber),2))
 	print 'Total Number of Control Points:',np.shape(ControlPoints)
 	#Start from the first master Point
 	CPpos=0
 	CNP=MasterPoints[0,2].astype(int)
 	#set up a phantom master point again
 	MasterPoints=np.concatenate((MasterPoints,MasterPoints[0,:].reshape(1,3)))
-	#Plotting for tests
-	test = plt.figure()
-	ax = test.add_subplot(111)
-	ax.plot(contourData[:,0],contourData[:,1]*-1,'-or')
-	plt.xlim(-0, 500)
-	plt.ylim(-500, 0)
-	plt.gca().set_aspect('equal', adjustable='box')
 
-	#axLine.scatter(Point[0],Point[1], facecolors='g', edgecolors='g')
-	#axLine.scatter(xPt,yPt, facecolors='r', edgecolors='r')
-	#axLine.plot([Point[0],xPt],[Point[1],yPt],color='r')
-	#axLine.axis('equal')
 	for i in range(len(MasterPoints)-1):
 		#set the slaveSegment size for the current slave segment.
 		segSize=distVector[i]/(seedNumber)
-		print 'DistVector',distVector[i],'seedNumber',seedNumber-1,'MasterNumber',len(MasterPoints),'segSize',segSize
+		#print 'DistVector',distVector[i],'seedNumber',seedNumber-1,'MasterNumber',len(MasterPoints),'segSize',segSize
 		#Initislise vaiables and arrays using the current control point (which is the master point for this segment)
-		Dist=0
-		MDist=0
+		Dist=MDist=nodeCount=0
+		print '\nMpoint:',MasterPoints[i,2],'CPpos:',CPpos,'CNP:',CNP
 		ControlPoints[CPpos,:]=MasterPoints[i,:2]
 		#increment to the first slave point
 		CPpos=CPpos+1
 		#looping through data points, while the the current node is not the next master point
 		while CNP!=MasterPoints[i+1,2]:
 			#If else to check if we are at the end of the array, to loop back to the beginning.
-			if CNP==(len(contourData)-1): 
+			if CNP==(len(contourData)-1):
 				target=0
 			else:
 				target=CNP+1
@@ -417,27 +409,35 @@ def seedSlavePoints(distVector,contourData,MasterPoints,seedNumber,ax):
 			#calculate the length of the data segment that we are on.
 			segDist=calculateLineLength(contourData[CNP,:],contourData[target,:])
 			MDist=MDist+segDist
-			print '\tCPpos',CPpos,'CNP',CNP,'SegDist',segDist,'Dist',Dist,'MDist',MDist
+			#print '\tCPpos',CPpos,'CNP',CNP,'SegDist',segDist,'Dist',Dist,'MDist',MDist
 
 			#if the current distance, plus the length of the data segment is larger then our current slaveSegment spacing. we will calculate where the slave seed should be placed, and what remains
-			if Dist+segDist > segSize:	
+			if Dist+segDist >= segSize:
+				print 'Mpoint:',MasterPoints[i,2],'CPpos:',CPpos,'CNP:',CNP
 				ControlPoints[CPpos,:],remainder = calculatePointPositionOnLine(segSize-Dist,segDist,contourData[CNP,:],contourData[target,:])
-				ax.plot(ControlPoints[CPpos,0],ControlPoints[CPpos,1]*-1,'ob')
-				print '\t\tplacing a seed point at position',CPpos,'Point Positions [',ControlPoints[CPpos,0],ControlPoints[CPpos,1],'], remainder:',remainder
+				nodeCount=nodeCount+1
+				#print '\t\tplacing a seed point at position',CPpos,'Point Positions [',ControlPoints[CPpos,0],ControlPoints[CPpos,1],'], remainder:',remainder
 				Dist=remainder
 				CPpos=CPpos+1
 				#on the offchance that the segment distance of the data points is huge, begger then our seed segment size, we nee to place multiple points on the data segment.
 				while remainder > segSize:
-					print '\t\t\trunning while loop'
-					ControlPoints[CPpos,:],remainder = calculatePointPositionOnLine((segDist-(remainder-segSize)),segDist,contourData[CNP,:],contourData[target,:])
-					ax.plot(ControlPoints[CPpos,0],ControlPoints[CPpos,1]*-1,'or')
-					CPpos=CPpos+1
+					if nodeCount==seedNumber-1:
+						remainder=0
+					else:
+						print 'running while loop: Remainder:', remainder, 'segSize:',segSize
+						print 'Mpoint:',MasterPoints[i,2],'CPpos:',CPpos,'CNP:',CNP
+						ControlPoints[CPpos,:],remainder = calculatePointPositionOnLine((segDist-(remainder-segSize)),segDist,contourData[CNP,:],contourData[target,:])
+						nodeCount=nodeCount+1
+						CPpos=CPpos+1
 
 				Dist=remainder
 			else:
 				Dist=Dist+segDist
 
-			CNP=target
+			if nodeCount==seedNumber-1:
+				CNP=MasterPoints[i+1,2].astype(int)
+			else:
+				CNP=target
 			#print 'CNP value:',CNP,'Distance',Dist
 
 	return ControlPoints
@@ -445,12 +445,12 @@ def seedSlavePoints(distVector,contourData,MasterPoints,seedNumber,ax):
 def calculatePointPositionOnLine(segSize,segDist,point1,point2):
 	#calculate the fraction along the line length that the seed point will lie
 	#frac=((segSize-segDist)/segSize)
-	frac=(segSize/segDist)	
+	frac=(segSize/segDist)
 	xPos=(point2[0]-point1[0])*frac + point1[0]
 	yPos=(point2[1]-point1[1])*frac + point1[1]
 	remainder=(1-frac)*segDist
-	print '\t\tRunning calculatePointPositionOnLine'
-	print '\t\tFrac value:',frac,'remainder ratio',(1-frac)
+	#print '\t\tRunning calculatePointPositionOnLine'
+	#print '\t\tFrac value:',frac,'remainder ratio',(1-frac)
 	return [xPos,yPos],remainder
 
 
@@ -459,12 +459,12 @@ def calculateDistanceVector(contourData,MasterPoints):
 	print 'Running calculateDistanceVector'
 	distVector=np.zeros(len(MasterPoints))
 	#Start from the first master Point, check that its not right at the end
+	#print 'Contour Data:\n', contourData
 	if MasterPoints[0,2]==(len(contourData)-1):
-		print 'god damit'
 		CNP=0
 	else:
 		CNP=MasterPoints[0,2].astype(int)+1
-	
+
 	MPcount=0
 	distVector[MPcount]=calculateLineLength(contourData[MasterPoints[0,2].astype(int),:],contourData[CNP,:])
 	#create a phantom node to stop the loop condition
@@ -481,10 +481,10 @@ def calculateDistanceVector(contourData,MasterPoints):
 			#print 'nodes (',CNP,') [',contourData[CNP,0],contourData[CNP,1],'] (',CNP+1,') [',contourData[CNP+1,0],contourData[CNP+1,1],'] Distance',distVector[MPcount]
 			CNP=CNP+1
 
-		print 'Distance',distVector[MPcount]
+		#print 'Distance',distVector[MPcount]
 		if (CNP==MasterPoints[MPcount+1,2]):
 			MPcount=MPcount+1
-			print 'MPcount',MPcount
+			#print 'MPcount',MPcount
 
 	return distVector
 
@@ -495,16 +495,28 @@ def calculateLineLength(Point1,Point2):
 
 def projectPointToLine(Line,Point):
 	print 'Running projectPointToLine'
-	#Calculate the gradient Mt (target) and constant Ct
-	Ct=(Line[0,1]*Line[1,0] - Line[1,1]*Line[0,0])/(Line[1,0]-Line[0,0])
-	Mt=(Line[0,1]-Ct)/Line[0,0]
-	#The gradient of the orthogonal line is the inverse of Mt
-	Ms=-1/Mt
-	#Given the point and the gradient, calculate the constant 
-	Cs=Point[1]-Point[0]*Ms
-	#Calculate the intercept between the two lines
-	xPt=(Cs-Ct)/(Mt-Ms)
-	yPt=Mt*xPt+Ct
+	#print 'Line:',Line,'Point1:',Point[0],'Point2',Point[1]
+	#Check if the line is vertical
+	if abs(Line[1,0]-Line[0,0])<0.0001:
+		print 'Vertical line detected'
+		xPt=Line[1,0]
+		yPt=Point[1]
+	elif abs(Line[1,1]-Line[0,1])<0.0001:
+		print 'Horizontal line detected'
+		xPt=Point[0]
+		yPt=Line[1,1]
+	else:
+		#Calculate the gradient Mt (target) and constant Ct
+		Ct=(Line[0,1]*Line[1,0] - Line[1,1]*Line[0,0])/(Line[1,0]-Line[0,0])
+		Mt=(Line[0,1]-Ct)/Line[0,0]
+		#The gradient of the orthogonal line is the inverse of Mt
+		Ms=-1/Mt
+		#Given the point and the gradient, calculate the constant
+		Cs=Point[1]-Point[0]*Ms
+		#Calculate the intercept between the two lines
+		xPt=(Cs-Ct)/(Mt-Ms)
+		yPt=Mt*xPt+Ct
+
 	#linePlot = plt.figure()
 	#axLine = linePlot.add_subplot(111)
 	#axLine.plot([Line[0,0],Line[1,0]],[Line[0,1],Line[1,1]])
@@ -544,15 +556,3 @@ def checkPointOnLineInterval(Line,Point):
 		#sanity break
 #		if pointPosition==3:
 #			lineCheck=True
-
-
-
-
-
-
-
-					
-
-
-
-
